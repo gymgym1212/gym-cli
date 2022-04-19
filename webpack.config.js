408a8edb5d296a5a0cba97d9bf6998b51857ad7c
Yeming Gu <51205903090@stu.ecnu.edu.cn>
2022-04-19 23:40:24 +0800

    init commit
    Signed-off-by: Yeming Gu <51205903090@stu.ecnu.edu.cn>

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // title: 'Development',
+            filename: 'index.html',
+            template: 'index.html',
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        publicPath: '/',
+    },
+}
